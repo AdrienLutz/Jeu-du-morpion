9c138d5832fdd445647cc5134f8d1d3d96213c0e
Arnaud Bravard <bravard.arnaud@gmail.com>
2023-04-04 10:17:35 +0200

    mise en place de la fonction ajout joueur en js

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 function validation () {
     const PLAYERONE = document.querySelector('#player1-name').value;
     const PLAYERTWO = document.querySelector('#player2-name').value;
-    // const SCOREONE = document.querySelector('#player1-score');
-    // SCOREONE.innerHTML = PLAYERONE;
-    player1-score.innerHTML = PLAYERONE;
+    const SCOREONE = document.querySelector('#player1-score');
+    SCOREONE.innerHTML = PLAYERONE;
     const SCORETWO = document.querySelector('#player2-score');
     SCORETWO.innerHTML = PLAYERTWO;
 }
@@ -14,4 +13,4 @@ function replacename ()  {
     document.querySelector('#btn-start-game').addEventListener('click', validation);
 }
 
-Window.addEventListener('load' , replacename );
+window.addEventListener('load' , replacename );
