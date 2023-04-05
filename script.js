50b1b4fcad4ae6745dfd25e0db7cf01a9d78c6b5
Stephane <stephane.marliacy@gmail.com>
2023-04-05 13:38:00 +0200

    Premiere condition de victoire

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function replacename() {
 
 function change_case_o(TOUR) {
     //let TOUR = document.querySelector('.btn1');
-    TOUR.innerHTML = '<p>O</p>';
+    TOUR.innerHTML = 'O';
     // let i = 0;
     // while(i<TOUR.length) {
     //     TOUR[i].innerHTML='<p>O</p>';
@@ -30,7 +30,7 @@ function change_case_o(TOUR) {
 
 function change_case_x(TOUR) {
     //let TOUR = document.querySelector('.btn1');
-    TOUR.innerHTML = '<p>X</p>';
+    TOUR.innerHTML = 'X';
     // let i = 0;
     // while(i<TOUR.length) {
     //     TOUR[i].innerHTML='<p>X</p>';
@@ -41,13 +41,14 @@ function change_case_x(TOUR) {
 function radio_player(evt) {
     console.log(evt);
     const RADIO1 = document.querySelector('input[name=radio]:checked').value;
-          
+    
     if(RADIO1 == 0) {
         change_case_o(evt.target);
         
     } else {
         change_case_x(evt.target);        
     }
+    victoire(); 
 }
 
 function onclick_grille() {
@@ -66,6 +67,50 @@ function onclick_grille() {
 
 
 
+
+//      RECHERCHE DE VAINQUEUR 
+
+
+function victoire() {
+    let resultat1 = document.querySelector('#idbtn1').innerHTML; 
+    let resultat2 = document.querySelector('#idbtn2').innerHTML;  
+    let resultat3 = document.querySelector('#idbtn3').innerHTML;
+    
+
+    if (
+              (resultat1 && resultat2 && resultat3 ==  "X") || (resultat1 && resultat2 && resultat3 ==  "O")
+            ) {
+              alert("Victoire");
+            }
+        
+               
+
+    
+        
+    
+
+
+//     const PLAYERTWO = document.querySelector('#player2-name').value;
+//     const SCOREONE = document.querySelector('#player1-score');
+//     SCOREONE.innerHTML = PLAYERONE;
+//     const SCORETWO = document.querySelector('#player2-score');
+//     SCORETWO.innerHTML = PLAYERTWO;
+//     document.querySelector('#players-names').classList.add("disabled");
+}
+
+
+
+// function rechercherVainqueur(grille) {
+//     if (
+//       case1.innerHTML ==  case2.innerHTML == case3.innerHTML
+//     ) {
+//       alert("Victoire");
+//       return true;
+//       console.log("ceci est le premier log" + case1.innerHTML);
+//     }
+
+//     console.log("ceci est le second log" + case1.innerHTML);
+//     }
 
 
 window.addEventListener('load', replacename);
